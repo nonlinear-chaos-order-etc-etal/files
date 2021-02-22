1d898111eeb158be644611766be8993adec5fc11
idk <hankhill19580@gmail.com>
2021-02-22 11:12:28 -0500

    releases/0.01a/

---
+++ mirror.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+
+RELEASELISTING=$(lynx -listonly -nonumbers -dump https://download.i2p2.de/releases/ | sed 's|https://download.i2p2.de/||g' )
+
+SCHEME='https://'
+
+SITE='download.i2p2.de/'
+
+for f in $RELEASELISTING; do
+#  echo $()
+#  lynx -listonly -nonumbers -dump $f
+  wget -r -np $SCHEME$SITE$f
+  cp -rv $SITE$f/* $f/
+  git add $f*
+  git commit -am "$f"
+  echo git push
+  exit
+done
