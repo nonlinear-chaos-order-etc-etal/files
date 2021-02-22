11da4a2e0ba4186fc8a923c7c5841ebd4d2ea452
idk <hankhill19580@gmail.com>
2021-02-22 11:14:08 -0500

    releases/0.01a/

--- mirror.sh
+++ mirror.sh
@@ -7,13 +7,10 @@ SCHEME='https://'
 SITE='download.i2p2.de/'
 
 for f in $RELEASELISTING; do
-#  echo $()
-#  lynx -listonly -nonumbers -dump $f
   wget -r -np -c $SCHEME$SITE$f
   mkdir -p $f/
   cp -rv $SITE$f/* $f/
   git add $f*
   git commit -am "$f"
-  echo git push
-  exit
+  git push
 done
